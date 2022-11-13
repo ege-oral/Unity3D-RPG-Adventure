1c4bfeccc15d9cad47b57a2196581d59a5fa3670
Ege Oral <egeoral@gmail.com>
2022-11-13 13:20:58 +0300

    Character stats added.

---
+++ Assets/Scripts/Stats/CharacterStats.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterStats : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth { get; private set; }
+
+    public Stat damage;
+    public Stat armor;
+
+    private void Awake() 
+    {
+        currentHealth = maxHealth;    
+    }
+
+    private void Update() 
+    {
+        if(Input.GetKeyDown(KeyCode.T))
+        {
+            TakeDamage(10);
+        }    
+    }
+
+    public void TakeDamage(int damage)
+    {
+        damage -= armor.GetValue();
+        damage = Mathf.Clamp(damage, 0, int.MaxValue);
+
+        currentHealth -= damage;
+        print(transform.name + " takes " + damage + " damge.");
+
+        if(currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    public virtual void Die()
+    {
+        Debug.Log(transform.name + " died.");
+    }
+
+}
---
+++ Assets/Scripts/Stats/PlayerStats.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : CharacterStats
+{
+    
+    void Start()
+    {
+        EquipmentManager.instance.onEquipmentChange += OnEquipmentChanged;
+    }
+
+    public void OnEquipmentChanged(Equipment newItem, Equipment oldItem)
+    {
+        if(newItem != null)
+        {
+            armor.AddModifier(newItem.armorModifier);
+            damage.AddModifier(newItem.damageModifier);
+        }
+
+        if(oldItem != null)
+        {
+            armor.RemoveModifier(oldItem.armorModifier);
+            damage.RemoveModifier(oldItem.damageModifier);
+        }
+    }
+    
+}
