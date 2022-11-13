e232784e8b5af7d0760e5b6b90160bfddd079a21
Ege Oral <egeoral@gmail.com>
2022-11-14 00:02:14 +0300

    Character combat script added.

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -2,11 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterStats))]
 public class Enemy : Interactable
 {
+    PlayerManager playerManager;
+    CharacterStats myStats;
+
+    private void Start() 
+    {
+        playerManager = PlayerManager.instance;
+        myStats = GetComponent<CharacterStats>();    
+    }
+    
     public override void Interact()
     {
         base.Interact();
-        // Attack
+        CharacterCombat playerCombat = playerManager.player.GetComponent<CharacterCombat>();
+
+        if(playerCombat != null)
+        {
+            playerCombat.Attack(myStats);
+        }        
     }
 }
